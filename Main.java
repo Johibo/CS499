b344cd30358f4f1686e623a68bc77cd2c9544a0e
Johibo <jaseabaugh4s@semo.edu>
2026-02-20 22:59:38 -0600

    Update Main.java
    This is to help function for the GUI

--- Main.java
+++ Main.java
@@ -1,41 +1,10 @@
-import auth.AuthService;
-import vault.VaultService;
-
-import java.util.Scanner;
+import ui.LoginFrame;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("=== Secure Password Manager Prototype ===");
-
-        System.out.print("Register Email: ");
-        String email = scanner.nextLine();
-
-        System.out.print("Master Password: ");
-        String password = scanner.nextLine();
-
-        AuthService.register(email, password);
-        System.out.println("Account created.\n");
-
-        System.out.print("Login Password: ");
-        String loginPass = scanner.nextLine();
-
-        if (!AuthService.login(email, loginPass)) {
-            System.out.println("Login failed.");
-            return;
-        }
-
-        System.out.println("Login successful!\n");
-
-        String generated = VaultService.generatePassword(16);
-        System.out.println("Generated Password: " + generated);
-
-        VaultService.addCredential(email, "example.com", "user123", generated);
-        System.out.println("Credential stored securely.\n");
-
-        System.out.println("=== Vault Contents ===");
-        VaultService.viewVault(email);
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new LoginFrame().setVisible(true);
+        });
     }
 }
